6d08684885d41038a56b77919a07f193a4afb67a
mlapeyre73 <marian.lapeyre@orange.fr>
2017-05-12 15:37:32 +0200

    adress

--- appPrincipale/models.py
+++ appPrincipale/models.py
@@ -19,6 +19,7 @@ class Objet(models.Model):
     nom = models.CharField(max_length=40)
     description = models.TextField(null=True, max_length=500, default ="Pas de description")
     code_postal= models.CharField(max_length=5,default="69100")
+    adresse = models.CharField(max_length=150, default="100 roger salengro")
     photo=models.ImageField(upload_to="photos/", default="photos/ryan_ygf9Uff.jpg")
     user=models.ForeignKey(User, on_delete=models.CASCADE, default="1")
 
@@ -28,5 +29,6 @@ class Comentaire(models.Model):
     content_type = models.ForeignKey(ContentType,default=Objet)
     object_id = models.PositiveIntegerField(default=1)
     content_object = GenericForeignKey('content_type', 'object_id')
+    user=models.ForeignKey(User, on_delete=models.CASCADE, default="1")
     def __str__(self):
         return "Commentaire de {0} sur {1}".format(self.contenu, self.content_object)
